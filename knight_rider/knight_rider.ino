702a892de340dc615da22f6461f254b1198fb466
shooter <shooter.hero@gmail.com>
2015-08-02 00:10:45 +0800

    knight_rider 001

---
+++ knight_rider/knight_rider.ino
@@ -0,0 +1,54 @@
+/*
+Note that LED 1 is LED[0] in sketch
+
+LED 1  |----+----|
+LED 2  :    |----+----|
+LED 3  :    :    |----+----|
+LED 4  :    :    :    |----+----|
+LED 5  :    :    :    :    |----+----|
+LED 6  :    :    :    :    :    |----+----|
+       :    :    :    :    :    :    :    :
+POTI   0    H   2H   3H   4H   5H   6H   1023  (H = 1023/7)
+---------------------------------------------
+For LED L:  |----+----|
+            :    :    :
+           H*L   :    :             Brightness:0
+               H(L+1) :             Brightness:255
+                     H(L+2)         Brightness:0
+
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+//
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+
+  ////////////////////
+
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+
+}
